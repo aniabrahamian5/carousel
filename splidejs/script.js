96a6b501e10877547f1e5e9b4bb5f88761bb1c87
aniabrahmian5 <abrahamianani5@gmail.com>
2025-07-18 18:17:02 +0400

    add splidejs

---
+++ splidejs/script.js
@@ -0,0 +1,74 @@
+let splideInstance
+
+function initSplide(options) {
+    const splideEl = document.getElementById('splideMain')
+    splideEl.style.display = 'block'
+
+    if (splideInstance) {
+        splideInstance.destroy(true)
+    }
+
+    splideInstance = new Splide('#splideMain', options)
+    splideInstance.mount()
+}
+
+function handleModeChange(mode) {
+    const configMap = {
+        'switch1Img': {
+            type: 'loop',
+            perPage: 1,
+            autoplay: true,
+            interval: 5000,
+        },
+        'switch3_1Img': {
+            type: 'loop',
+            perPage: 3,
+            perMove: 3,
+            autoplay: true,
+            interval: 5000,
+            breakpoints: {
+            768: { perPage: 2 },
+            480: { perPage: 1 },
+            }
+        },
+        'switch3_3Img': {
+            type: 'loop',
+            perPage: 3,
+            perMove: 1,
+            autoplay: true,
+            interval: 5000,
+            breakpoints: {
+            768: { perPage: 2 },
+            480: { perPage: 1 },
+            }
+        }
+    }
+
+    initSplide(configMap[mode])
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const modeRadios = document.querySelectorAll('input[name="mode"]')
+    const secondModeRadios = document.querySelectorAll('input[name="secondMode"]')
+    const toggledRadiosDiv = document.getElementById('toggledRadios')
+
+    modeRadios.forEach(radio => {
+        radio.addEventListener('change', () => {
+            if (radio.value === 'switch3Img') {
+            toggledRadiosDiv.style.display = 'block'
+            } else {
+            toggledRadiosDiv.style.display = 'none'
+            handleModeChange(radio.value)
+            }
+        })
+    })
+
+    secondModeRadios.forEach(radio => {
+        radio.addEventListener('change', () => {
+            handleModeChange(radio.value)
+        })
+    })
+    
+    const defaultRadio = document.querySelector('input[name="mode"]:checked')
+    if (defaultRadio) handleModeChange(defaultRadio.value)
+})
